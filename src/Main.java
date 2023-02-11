c4f66cfed1c7bce4d4485e03d06edc5448e3d914
Darya Tarasova <kuroifreya@gmail.com>
2023-02-11 08:49:27 +0300

    Code reformatted

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ public class Main {
         double replenishment = 1010;
         int bonus;
 
-        bonus = (replenishment > 1000) ? (int)(replenishment / 100) : 0;
+        bonus = (replenishment > 1000) ? (int) (replenishment / 100) : 0;
         balance = balance + replenishment + bonus;
 
         System.out.println("Ваш баланс: " + balance + "р.");
         if (bonus > 0) {
             System.out.println("Вы пополнили баланс больше, чем на тысячу рублей, поэтому мы добавили к сумме " + bonus + " р. :)");
-            }
-            else {
+        } else {
             System.out.println("Вы можете получить бонус, если пополните баланс больше, чем на тысячу рублей!");
-            }
+        }
     }
 }
